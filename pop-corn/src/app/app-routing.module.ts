3a660603851363482ae367f2c1bd30e8c26ecf40
ivanletralok <ivanletralok@gmail.com>
2022-08-10 21:06:14 -0500

    feat: modular project, lazy load

--- pop-corn/src/app/app-routing.module.ts
+++ pop-corn/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes =
+  [
+    {
+      path:'products',
+      loadChildren: () => import('./products/product.module').then(m => m.ProductModule)
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ pop-corn/src/app/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthroutingModule } from './auth-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthroutingModule
+  ]
+})
+export class AuthModule { }
